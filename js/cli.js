22f0a40ccd2f1b2b8fa33e7dc0869278ec0ae559
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-05-18 09:09:07 -0700

    add prompt package

--- js/cli.js
+++ js/cli.js
@@ -1,7 +1,13 @@
-var pingPong = require('./ping-pong.js').pingPong;
+// var pingPong = require('./ping-pong.js').pingPong;
+//
+// console.log('Ping-ponging up to 100!');
+// var result = pingPong(100);
+// result.forEach(function(element) {
+//   console.log(element);
+// });
 
-console.log('Ping-ponging up to 100!');
-var result = pingPong(100);
+var goal = prompt('What number would you like me to ping-pong up to?');
+var result = pingPong(goal);
 result.forEach(function(element) {
   console.log(element);
 });
