0c4f891d16c39849cea84915e88d527e29b93542
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-05-18 09:36:19 -0700

    add gulp

--- js/cli.js
+++ js/cli.js
@@ -1,13 +1,10 @@
-// var pingPong = require('./ping-pong.js').pingPong;
-//
-// console.log('Ping-ponging up to 100!');
-// var result = pingPong(100);
-// result.forEach(function(element) {
-//   console.log(element);
-// });
+var pingPong = require('./ping-pong.js').pingPong;
+var prompt = require('prompt');
+prompt.start();
 
-var goal = prompt('What number would you like me to ping-pong up to?');
-var result = pingPong(goal);
-result.forEach(function(element) {
-  console.log(element);
+prompt.get('goal', function(err, result) {
+  var result = pingPong(result.goal);
+  result.forEach(function(element) {
+    console.log(element);
+  });
 });
