107db937df73931f2a94bbb97bb9b2c32b667b34
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-05-18 14:34:38 -0700

    concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
-gulp.task('jsBrowserify', function() {
-  return browserify({entries: ['./js/browser.js']})
+gulp.task('concatInterface', function(){
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./temp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({entries: ['./tmp/allConcat.js']})
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
