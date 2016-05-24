1c45b41ec4a668e531f3e3acc12f832223e4c03b
Alex Hagge and Kevin Deganos <kevin@>
2016-05-24 09:43:28 -0700

    connected server

---
+++ js/weather-interface.js
@@ -0,0 +1,12 @@
+var apiKey = "12eb4aded9a1c4429068427521db3246";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -19,3 +19,20 @@ $(document).ready(function() {
     $("#solution").prepend("<p>Thank you, " + email + "has been added to our list!</p>");
   });
 });
+
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+var apiKey = "12eb4aded9a1c4429068427521db3246";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
